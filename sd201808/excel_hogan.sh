6b132f2bc16f7d3b98e35c25c18b42cfd07ecc29
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-09-13 17:43:36 +0900

    fix

--- sd201808/excel_hogan.sh
+++ sd201808/excel_hogan.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # テキストをスプレッドシートのセル一つ一つに1文字ずつ入れる
-cat ../ShellGeiData/sd201808/excel_hogan.txt | sed 's/./&_/g' | sed 's/"/""""/g' | sed 's/,/","/g' | tr _ , > hoge.csv
+cat ../ShellGeiData/sd201808/excel_hogan.txt | sed 's/./&_/g' | sed 's/"/""""/g' | sed 's/,/","/g' | tr _ ,
 
