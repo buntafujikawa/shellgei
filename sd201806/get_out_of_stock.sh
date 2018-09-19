ad2c66fad00676bcdf3b13e236d86493e33760cf
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-09-19 15:38:48 +0900

    fix

--- sd201806/get_out_of_stock.sh
+++ sd201806/get_out_of_stock.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+# 売り切れているSDの一覧を取得
 nkf -wLux backnumber| grep 品切 -B3 | pandoc -f html -t plain
 
--- sd201806/get_weather_forecast.sh
+++ sd201806/get_weather_forecast.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+# 静岡の天気を調べる
 cat yoho_327.html| sed -ne '/class="forecast"/,$p' | egrep "float: left|img" | pandoc -f html -t plain
 
